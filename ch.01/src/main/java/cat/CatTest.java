f5a2d82686303b32bdeaa96092c480fc01d6e017
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2022-04-09 18:20:23 +0900

    백준 알고리즘 연습 및 공부 단계별 풀어보기 ch.01

---
+++ ch.01/src/main/java/cat/CatTest.java
@@ -0,0 +1,13 @@
+package cat;
+
+public class CatTest {
+    public static void main(String[] args) {
+        System.out.println(
+
+                "\\    /\\\n"   +
+                " )  ( ')\n" +
+                "(  /  )\n"  +
+                "\\(__)|"
+        );
+    }
+}
---
+++ ch.01/src/main/java/dog/DogTest.java
@@ -0,0 +1,11 @@
+package dog;
+
+public class DogTest {
+    public static void main(String[] args){
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"'    |\n"  +
+                "||_/=\\\\__|"   );
+    }
+}
